34e51a968efd45faec1f6f7e273715acce1de72a
andkcho <{andkcho}@users.noreply.github.com>
2018-03-07 22:59:08 -0700

    first commit

---
+++ db/schema.sql
@@ -0,0 +1,16 @@
+
+-- DROP DATABASE IF EXISTS burgers_db;
+CREATE DATABASE zgylzqxjfq0uh48o;
+-- CREATE DATABASE burgers_db;
+
+USE zgylzqxjfq0uh48o;
+-- USE burgers_db;
+
+CREATE TABLE burgers
+(
+    id int NOT NULL AUTO_INCREMENT,
+	burger_name varchar (255) NOT NULL,
+	devoured boolean,
+	date TIMESTAMP,
+	PRIMARY KEY (id)
+);
